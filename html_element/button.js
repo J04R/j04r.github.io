21c6885726aa58a4af31adb231940b706f246fea
Joar Löfstaf <jolo0928@storsjogymnasiet.se>
2023-09-21 11:21:07 +0200

    Script Site

--- html_element/button.js
+++ html_element/button.js
@@ -1,3 +1,5 @@
 
 
 function buttonPress(){alert("Hello there! This was the results of pressing the button! You get a free cookie! :D")}
+//Java script yes yes very nice mhm interesting
+console.log("An Error AAAAAAAA")
