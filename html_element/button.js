0fb1f2559e98c6d1f2ccd841a5f319897990f110
Joar Löfstaf <jolo0928@storsjogymnasiet.se>
2023-09-25 12:32:42 +0200

    thirdcommit

--- html_element/button.js
+++ html_element/button.js
@@ -1,20 +1,33 @@
 function buttonPress() {alert("You shouldn't be here..")
-alert("I warned you not to click the button")
-alert("And you did..")
-alert("Just exit the webpage and we can move on..")
-alert("Oi! Stop!")
-alert("Stop pressing OK!")
-alert("Im not gonna let you continue!")
-alert("...")
-alert("...")
-alert("...")
-alert("...")
-alert("...")
+alert("You have doomed us all..")
+alert("WHY!? It said 'Do not click!' And you clicked it..")
+alert("You have one more chance to save us...")
+alert("All you need to do is exit the webpage..")
+alert("Stop pressing it!")
+alert("You don't know what will happen..")
+alert("Please stop...")
 alert("Stop!")
-alert("Im actually serious, dont!")
-alert("You are not giving up, are you?")
-alert("Do you really want to see?")
-alert("Alright fine but im telling you now its not good..")
-alert("here we go..")
+alert("You will never be the same..")
+alert("You wont stop, will you?")
+alert("Ill show you okay!")
+alert("But im telling you now you will regret this.")
+alert("Dont say i didnt warn you..")
 window.location.href = "https://youtu.be/xvFZjo5PgG0";
 }
+
+onload=function timer(){
+    var sec = 60;
+    var timer = setInterval(function(){
+        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+setTimeout(function() { 
+    alert("Congrats! You didn't let your intrusive thoughts take over! Here is your reward! Press OK to contnue!")
+    window.location.href = "https://youtu.be/LCrCCgjdKx8"; }, 61000 );
+
+ 
