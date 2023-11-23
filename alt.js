d009db86404c93c8672e8283d4c450a30b6edaa3
J04R <joarlofstaf@gmail.com>
2023-11-23 18:58:04 +0100

    commit2

--- alt.js
+++ alt.js
@@ -10,7 +10,7 @@ function updateText() {
 
     setInterval(function () {
         // Your array of texts
-        const texts = ["Visste du att chile är en av de svåraste länderna att spela som i supremacy 1914? - Ruben", "Du gjorde mig nästan till rostbiff! - Ruben ", "Jag gillar penisar. - Ruben", " - Ruben", "Man måste tänka på tanken eftersom det är tanken som räknas när man tankar. - Ruben", "text 6", "text 7", "text 8", "text 9",];
+        const texts = ["Visste du att chile är en av de svåraste länderna att spela som i supremacy 1914? - Ruben", "Kan man vara så kort så att man inte kan nå sin egen längd? - Ruben ", "Hur mycket power behöver man i Rise Of Kingdoms för att bli av med ett mord? - Ruben", " Hur gammal blir man ifall man åldras tillräckligt länge? - Ruben", "Man måste tänka på tanken eftersom det är tanken som räknas när man tankar. - Ruben", "Om man donerar till en fond så donerar den fonden det du donerade. - Ruben", "text 7", "text 8", "text 9",];
 
         // Get a random text from the array
         const newText = texts[Math.floor(Math.random() * texts.length)];
